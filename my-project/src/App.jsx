e5bdab126988299bd3f546020521379db14acde4
zartax95 <adrienvilb@msn.com>
2024-04-16 16:55:57 +0200

    sorted array

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,7 +3,8 @@ import { Match } from "./utils/models";
 import Band from "./components/Band";
 
 export default function App() {
-  return matchs.map((item) => {
+  const sortedMatchs = matchs.sort((a, b) => new Date(new Match(a).USA_DATE() ) + new Date(new Match(b).USA_DATE()));
+  return sortedMatchs.map((item) => {
     const match = new Match(item);
     return <Band key={match.number} match={match} />;
   });
