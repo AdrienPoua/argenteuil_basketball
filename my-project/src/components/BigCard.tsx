a7ea4535d3525d67f7e07fb3521e527df812f4ca
zartax95 <adrienvilb@msn.com>
2024-04-17 17:56:38 +0200

    news en cours

---
+++ my-project/src/components/BigCard.tsx
@@ -0,0 +1,57 @@
+import React, { useRef, useEffect } from "react";
+
+type BigCardProps = { url: string; img: string; title: string; date: string };
+
+export const BigCard = ({ url, img, title, date }: Readonly<BigCardProps>) => {
+  const card = useRef<HTMLAnchorElement>(null);
+  const image = useRef<HTMLImageElement>(null);
+  useEffect(() => {
+    
+    const effect = "transition duration-500 ease-in-out scale-125".split(" ");
+
+    const handleMouseEnter = () => {
+      if (image.current) {
+        image.current.classList.add(...effect);
+      }
+    };
+
+    const handleMouseLeave = () => {
+      if (image.current) {
+        image.current.classList.remove(...effect);
+      }
+    };
+    if (card.current) {
+      card.current.addEventListener("mouseenter", handleMouseEnter);
+      card.current.addEventListener("mouseleave", handleMouseLeave);
+    }
+
+    return () => {
+      if (card.current) {
+        card.current.removeEventListener("mouseenter", handleMouseEnter);
+        card.current.removeEventListener("mouseleave", handleMouseLeave);
+      }
+    };
+  }, [card]);
+  return (
+    <a
+      ref={card}
+      href={url}
+      className='flex flex-col bg-black rounded-3xl overflow-hidden'
+      style={{ aspectRatio: "1/1" }}
+    >
+      <div className='relative h-full w-full'>
+        <img
+          ref={image}
+          src={img}
+          alt={title}
+          className='rounded-t-3xl w-full max-h-full object-cover'
+        />
+        <div className='absolute inset-0 bg-black opacity-40'></div>
+        <div className='absolute inset-x-0 bottom-5 text-white gap-4'>
+          <div className='text-lg font-black ps-5 mb-3'>{title}</div>
+          <div className='text-sm ps-5 pb-5'>{date}</div>
+        </div>
+      </div>
+    </a>
+  );
+};
--- my-project/src/components/News.tsx
+++ my-project/src/components/News.tsx
@@ -1,50 +1,7 @@
 import { NewsProps } from "../utils/types.ts";
 import React, { useEffect, useRef } from "react";
+import { BigCard } from "./BigCard";
 
-const BigCard = ({url, img, title, date}: Readonly<{url: string, img: string, title: string, date: string}>) => {
-  const card = useRef<HTMLAnchorElement>(null);
-  const image = useRef<HTMLImageElement>(null);
-
-  useEffect(() => {
-    const effect = "transition duration-500 ease-in-out scale-125".split(" ");
-  
-    const handleMouseEnter = () => {
-      if (image.current) {
-        image.current.classList.add(...effect);
-      }
-    };
-  
-    const handleMouseLeave = () => {
-      if (image.current) {
-        image.current.classList.remove(...effect);
-      }
-    };
-    if (card.current) {
-      card.current.addEventListener('mouseenter', handleMouseEnter);
-      card.current.addEventListener('mouseleave', handleMouseLeave);
-    }
-
-    return () => {
-      if (card.current) {
-        card.current.removeEventListener('mouseenter', handleMouseEnter);
-        card.current.removeEventListener('mouseleave', handleMouseLeave);
-      }
-    };
-  }, [card]);
-
-  return (
-    <a ref={card} href={url} className="flex flex-col bg-black rounded-3xl overflow-hidden" style={{ aspectRatio: '1/1' }}>
-      <div className="relative h-full w-full">
-        <img ref={image} src={img} alt={title} className="rounded-t-3xl w-full max-h-full object-cover" />
-        <div className="absolute inset-0 bg-black opacity-40"></div>
-        <div className="absolute inset-x-0 bottom-0 p-4 text-white">
-          <div className="text-lg font-bold">{title}</div>
-          <div className="text-sm">{date}</div>
-        </div>
-      </div>
-    </a>
-  );
-};
 
 
 export default function News({ mainNews, secondaryNews, othersNews }: Readonly<NewsProps>) {
@@ -52,9 +9,9 @@ console.log("🚀 ~ News ~ secondaryNews:", secondaryNews)
 
   return (
     <div className='flex flex-col w-100 ' >
-      <div className='flex justify-between mb-5 text-red-700 ' >
-        <div className='font-bold text-3xl'>A l'affiche </div>
-        <div className=''>All news </div>
+      <div className='flex justify-between mb-5 uppercase items-center py-5 ' >
+        <div className='font-black text-5xl '>Latest News </div>
+        <a href="/" className=' underline font-black basis-3/12 text-end '>All news </a>
       </div>
       <div className='flex gap-5'>
         <div className='flex-col w-1/2'>
