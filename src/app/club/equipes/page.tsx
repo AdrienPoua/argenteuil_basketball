3012f58e6889f7d1d658cd913b1f0b2152c50ff3
zartax95 <adrienvilb@msn.com>
2024-05-17 10:44:27 +0200

    correction taille card team

--- src/app/club/equipes/page.tsx
+++ src/app/club/equipes/page.tsx
@@ -2,10 +2,12 @@
 import teamsData from "@/data/teams.json";
 import playersData from "@/data/players.json";
 import { MemberFactory } from "@/factories";
-import { TeamType, AdherentType, PlayerType } from "@/types";
+import { AdherentType, PlayerType } from "@/types";
 import { Team, Player } from "@/models";
 import TeamCard from "@/components/Card";
 import CardLayout from "@/components/layouts/CardLayout";
+import { v4 as uuidv4 } from "uuid";
+
 
 export default function Index() {
   const isPlayer = (member: AdherentType): member is PlayerType => {
@@ -22,9 +24,9 @@ export default function Index() {
 console.log(teams)  
   return (
     <CardLayout pageTitle='Nos équipes '>
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col grow gap-5">
       {teams.map((team, index) => (
-        <TeamCard key={index} data={team} />
+        <TeamCard key={uuidv4()} data={team} />
       ))}
       </div>
     </CardLayout>
