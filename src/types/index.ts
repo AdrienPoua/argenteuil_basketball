dff38c27c75b86327ed9ce0cfd3bf0f00327b2e4
zartax95 <adrienvilb@msn.com>
2024-07-15 14:11:55 +0200

    refactor : clean some folders

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,10 @@
-import LeadershipType from "@/types/LeaderShipType";
+import LeadershipType from "@/types/LeadershipType";
 import GymType from "@/types/GymType";
 import MemberType from "@/types/MemberType";
 import TeamType from "@/types/TeamType";
 import TrainingType from "@/types/TrainingType";
 import SubItemType from "@/types/SubItemType";
 import NavItemType from "@/types/NavItemType";
-import NewsType from "@/types/NewsType";
 import { ClubTeam, CompetitionData, CompetitionTeams, CompetitionType, ClubType } from "@/types/APITypes";
 
-export type { LeadershipType, GymType, MemberType, NewsType, TeamType, TrainingType, SubItemType, NavItemType, ClubTeam, CompetitionData, CompetitionTeams, CompetitionType, ClubType };
+export type { LeadershipType, GymType, MemberType, TeamType, TrainingType, SubItemType, NavItemType, ClubTeam, CompetitionData, CompetitionTeams, CompetitionType, ClubType };
