1e9157047d7969121b4d2f3d33f4ce1973e146be
zartax95 <adrienvilb@msn.com>
2024-08-30 10:02:52 +0200

    add instructions component

---
+++ src/components/Instructions.tsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from "@mui/material";
+import { ReactElement } from "react";
+
+const Instructions = ({ children }: { children: React.ReactNode }) : ReactElement=> {
+    return (
+        <Box className="border-2 border-primary border-dashed p-10 rounded-xl relative mb-10">
+            <Typography variant="h3" className="text-primary absolute top-0 left-10 z-10 transform -translate-y-1/2 bg-black">
+                Instructions
+            </Typography>
+            <Box className="flex flex-col gap-4">
+                {children}
+            </Box>
+        </Box>
+    );
+}
+
+export default Instructions;
