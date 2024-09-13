da4432fef785985886945979a44d194d47439e9e
zartax95 <adrienvilb@msn.com>
2024-09-13 13:52:28 +0200

    refactor : useFetchMatchs

--- src/app/(base)/matchs/calendrier/page.tsx
+++ src/app/(base)/matchs/calendrier/page.tsx
@@ -1,31 +1,26 @@
 "use client";
+import Info from "@/components/Info";
 import H1 from "@/components/H1";
 import Schedule from "@/components/Schedule";
 import { Box } from "@mui/material";
+import Adapter from '@/utils/adapters/matchs/fromDBforModel';
 import Match from "@/utils/models/Match";
 import useFetchMatchs from "@/utils/hooks/fetchDatabase/useFetchMatchs";
 import FetchFeedback from "@/components/FetchFeedback";
-import { SAISON_MONTH_ORDER } from "@/utils/magicNumber";
 
 
 
 export default function Index() {
   const { data, isLoading, error } = useFetchMatchs();
-  const matchs = data?.map((match) => new Match(match))
-  const matchsByMonth = matchs && Match.groupByMonth(matchs)
-
   return (
-    <>
-      <H1> Calendrier des matchs </H1>
-      <FetchFeedback isLoading={isLoading} error={error} data={data}>
+    <FetchFeedback isLoading={isLoading} error={error} data={data}>
+      <>
+        <H1> Calendrier des matchs </H1>
         <Box className="flex flex-col max-w-[80%] mx-auto gap-10">
-          {matchsByMonth && Object.keys(matchsByMonth)
-            .sort((a, b) => SAISON_MONTH_ORDER.indexOf(a) - SAISON_MONTH_ORDER.indexOf(b))
-            .map((month) => (
-              <Schedule key={month} matchs={matchsByMonth[month]} title={month} />
-            ))}
+          {data && Object.keys(data).map((month) => (
+            <Schedule key={month} matchs={data[month]} title={month} />
+          ))}
         </Box>
-      </FetchFeedback >
-    </>
-  )
+      </>
+    </FetchFeedback>)
 }
--- src/utils/hooks/fetchDatabase/useFetchMatchs.ts
+++ src/utils/hooks/fetchDatabase/useFetchMatchs.ts
@@ -3,6 +3,9 @@ import { useQuery } from "react-query";
 import { getMatchs } from "@/lib/mongo/controllers/matchs";
 import { ValidateWithZod } from "@/lib/zod/utils/index";
 import { SDatabase } from "@/lib/zod/schemas";
+import Match from "@/utils/models/Match";
+import { SAISON_MONTH_ORDER } from "@/utils/magicNumber";
+import { inflateRaw } from "zlib";
 
 const fetchMatchs = async () => {
   const matchs = await getMatchs();
@@ -12,5 +15,7 @@ const fetchMatchs = async () => {
 
 export default function useFetchMatchs() {
   const { data, isLoading, error } = useQuery(["matchs"], fetchMatchs);
-  return { data, isLoading, error };
+  const matchs = data?.map((match) => new Match(match));
+  const matchsByMonth = matchs && Match.groupByMonth(matchs);
+  return { data: matchsByMonth, isLoading, error };
 }
