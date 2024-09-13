a4d8f5f84390196b32b7cb67b11a0c555a0a6e4a
zartax95 <adrienvilb@msn.com>
2024-09-13 13:52:28 +0200

    refactor : useFetchMatchs

--- src/app/(base)/matchs/calendrier/page.tsx
+++ src/app/(base)/matchs/calendrier/page.tsx
@@ -2,30 +2,23 @@
 import H1 from "@/components/H1";
 import Schedule from "@/components/Schedule";
 import { Box } from "@mui/material";
-import Match from "@/utils/models/Match";
 import useFetchMatchs from "@/utils/hooks/fetchDatabase/useFetchMatchs";
 import FetchFeedback from "@/components/FetchFeedback";
 import { SAISON_MONTH_ORDER } from "@/utils/magicNumber";
 
-
-
 export default function Index() {
-  const { data, isLoading, error } = useFetchMatchs();
-  const matchs = data?.map((match) => new Match(match))
-  const matchsByMonth = matchs && Match.groupByMonth(matchs)
-
+  const { data : plannings, isLoading, error } = useFetchMatchs();
   return (
-    <>
-      <H1> Calendrier des matchs </H1>
-      <FetchFeedback isLoading={isLoading} error={error} data={data}>
+    <FetchFeedback isLoading={isLoading} error={error} data={plannings}>
+      <>
+        <H1> Calendrier des matchs </H1>
         <Box className="flex flex-col max-w-[80%] mx-auto gap-10">
-          {matchsByMonth && Object.keys(matchsByMonth)
+          {plannings && Object.keys(plannings)
             .sort((a, b) => SAISON_MONTH_ORDER.indexOf(a) - SAISON_MONTH_ORDER.indexOf(b))
             .map((month) => (
-              <Schedule key={month} matchs={matchsByMonth[month]} title={month} />
+              <Schedule key={month} matchs={plannings[month]} title={month} />
             ))}
         </Box>
-      </FetchFeedback >
-    </>
-  )
+      </>
+    </FetchFeedback>)
 }
