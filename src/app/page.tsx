e09ea6b9e1eff69ce2ef822cbd5da1105bfd2c27
zartax95 <adrienvilb@msn.com>
2024-11-07 16:00:49 +0100

    adjust homepage card height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,25 +55,25 @@ const HeroSection = () => {
 function PostsWrapper() {
   const { leftPostOnHomePage, rightPostOnHomePage, postsOnHomePage } = useSanity()
   return (
-    <div className="px-4">
-      <div className="flex flex-wrap -mx-4 mb-10">
-        <div className="w-full md:w-1/2 px-4 mb-8 md:mb-0">
+    <div className=" mb-20">
+      <div className="flex flex-wrap gap-6 -mx-4 mb-10">
+        <div className="w-full md:w-[calc(50%-24px)] ">
           {leftPostOnHomePage && (
-            <div className="sticky top-10 h-[1000px] max-h-[80vh]">
+            <div className="sticky top-16 h-[1000px] max-h-[80vh]">
               <CustomCard post={leftPostOnHomePage} />
             </div>
           )}
         </div>
-        <div className="w-full md:w-1/2 px-4 ">
+        <div className="w-full md:w-[calc(50%-24px)] ">
           <div className="mb-8 h-[1000px] max-h-[80vh] ">
             {rightPostOnHomePage && (
               <CustomCard post={rightPostOnHomePage} />
             )}
           </div>
-          <div className="flex flex-wrap -mx-2 ">
+          <div className="flex flex-wrap gap-6 ">
             {postsOnHomePage?.slice(0, MAX_POSTS_ON_HOME_PAGE).map((post: SanityDocument) => (
-              <div key={post._id} className="w-full sm:w-1/2 px-2 mb-4 h-96 max-h-[50vh]">
-                <CustomCard post={post} />
+              <div key={post._id} className="w-full md:w-[calc(50%-12px)] mb-4 md:h-[700px] max-h-[50vh]">
+                <CustomCard post={post} small={true} />
               </div>
             ))}
           </div>
@@ -84,7 +84,7 @@ function PostsWrapper() {
 }
 
 
-const CustomCard = ({ post }: { post: SanityDocument }): ReactElement => {
+const CustomCard = ({ post, small }: { post: SanityDocument, small?: boolean }): ReactElement => {
   const { Image: postImage, title, publishedAt: date, slug } = post;
   const formatedDate = new Date(date).toLocaleDateString('fr-FR', {
     month: "long",
@@ -95,12 +95,12 @@ const CustomCard = ({ post }: { post: SanityDocument }): ReactElement => {
   return (
     <Link href={`/actualites/${slug.current}`} className="flex size-full">
       <Card className="group rounded-3xl overflow-hidden flex flex-col grow border-none">
-        <CardContent className="relative overflow-hidden grow">
+        <CardContent className={`relative overflow-hidden grow ${small ? "hidden md:block" : ""} `}>
           <Image
             src={urlFor(postImage).url()}
             alt={title}
             layout="fill"
-            className="group-hover:scale-110 transition-transform duration-300 size-full"
+            className="group-hover:scale-110 transition-transform duration-300 size-full display-hidden"
           />
           <div className="absolute inset-0 bg-black bg-opacity-50" />
         </CardContent>
