28dadd92f7f8f96d3d0db7cde748e52ad8d9eb73
zartax95 <adrienvilb@msn.com>
2024-12-18 15:56:31 +0100

    add transporter builder class for nodemailer

--- src/services/nodemailer/transporter.ts
+++ src/services/nodemailer/transporter.ts
@@ -1,20 +1,7 @@
 "use server";
-import nodemailer from "nodemailer";
 
-const { MAILGUN_API_KEY, EMAIL_PORT, EMAIL_HOST } = process.env;
+import Transporter from "@/models/NodemailerTransporter";
 
-if (!MAILGUN_API_KEY || !EMAIL_PORT || !EMAIL_HOST) {
-  throw new Error(
-    "Veuillez configurer les variables d'environnement, MAILGUN_API_KEY, EMAIL_PORT et EMAIL_HOST",
-  );
-}
-
-export const transporter = nodemailer.createTransport({
-  host: EMAIL_HOST,
-  port: parseInt(EMAIL_PORT, 10),
-  secure: false, // false pour TLS
-  auth: {
-    user: "postmaster@email.argenteuilbasketball.com",
-    pass: MAILGUN_API_KEY,
-  },
-});
+export const transporter = new Transporter.Builder()
+  .build()
+  .createTransporter();
