5eb3d26e5daf5d1d63dbafe9c2ecda27377d02c0
zartax95 <adrienvilb@msn.com>
2025-01-09 11:49:29 +0100

    styles : correspondant card

--- src/app/(admin)/dashboard/correspondants/components/Card.tsx
+++ src/app/(admin)/dashboard/correspondants/components/Card.tsx
@@ -1,12 +1,24 @@
 "use client"
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
-import { Pencil, Trash2, X } from 'lucide-react'
+import { Pencil, Trash2, X, Mail, Phone } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { deleteClub } from "../actions/server.actions"
 import { CardPropsType, PropsType } from "../types/card.types"
 import { useState } from "react"
 import Form from "./Form"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 export default function Index({ data }: Readonly<PropsType>) {
     const [isEditing, setIsEditing] = useState(false)
@@ -17,49 +29,69 @@ export default function Index({ data }: Readonly<PropsType>) {
 }
 
 export function ClubCard({ data, setIsEditing }: Readonly<CardPropsType>) {
+    const handleDelete = async () => {
+        await deleteClub(data.id);
+    };
+
     return (
-        <Card className="w-full max-w-md p-5 font-secondary hover:shadow-lg transition-shadow text-muted-foreground">
-            <CardHeader className="flex flex-col gap-2 mb-5 relative">
-                <CardTitle className="text-xl uppercase w-full bg-primary text-primary-foreground py-2 px-4 rounded-md text-center shadow-sm">
-                    {data.libelle}
-                </CardTitle>
-                <div className="absolute top-0 right-0 flex gap-2">
-                    <Button
-                        variant="destructive"
-                        size="icon"
-                        onClick={() => deleteClub(data.id)}
-                        aria-label={`Supprimer le club ${data.libelle}`}
-                    >
-                        <Trash2 className="h-4 w-4" />
-                    </Button>
-                    <Button
-                        variant="outline"
-                        size="icon"
-                        onClick={() => setIsEditing(true)}
-                        aria-label={`Modifier le club ${data.libelle}`}
-                    >
-                        <Pencil className="h-4 w-4" />
-                    </Button>
+        <Card className="w-full max-w-md font-secondary hover:shadow-lg transition-all duration-300 text-muted-foreground">
+            <CardHeader className="relative p-6">
+                <div className="flex justify-between items-center">
+                    <CardTitle className="text-2xl font-bold text-background">
+                        {data.libelle}
+                    </CardTitle>
+                    <div className="flex gap-2">
+                        <AlertDialog>
+                            <AlertDialogTrigger asChild>
+                                <Button
+                                    variant="destructive"
+                                    size="icon"
+                                    aria-label={`Supprimer le club ${data.libelle}`}
+                                >
+                                    <Trash2 className="h-4 w-4" />
+                                </Button>
+                            </AlertDialogTrigger>
+                            <AlertDialogContent>
+                                <AlertDialogHeader>
+                                    <AlertDialogTitle>Êtes-vous sûr de vouloir supprimer ce club ?</AlertDialogTitle>
+                                    <AlertDialogDescription>
+                                        Cette action ne peut pas être annulée. Cela supprimera définitivement le club {data.libelle} et toutes les données associées.
+                                    </AlertDialogDescription>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter>
+                                    <AlertDialogCancel>Annuler</AlertDialogCancel>
+                                    <AlertDialogAction onClick={handleDelete}>Confirmer la suppression</AlertDialogAction>
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
+                        <Button
+                            size="icon"
+                            onClick={() => setIsEditing(true)}
+                            aria-label={`Modifier le club ${data.libelle}`}
+                        >
+                            <Pencil className="h-4 w-4" />
+                        </Button>
+                    </div>
                 </div>
             </CardHeader>
-            <CardContent className="p-0">
-                <div className="space-y-6">
-                    <div className="flex items-center gap-3 p-2 bg-muted/50 rounded-md">
-                        <Badge className="whitespace-nowrap">
-                            Email
-                        </Badge>
-                        <p className="text-sm font-medium">
-                            {data.email}
-                        </p>
-                    </div>
-                    <div className="flex items-center gap-3 p-2 bg-muted/50 rounded-md">
-                        <Badge className="whitespace-nowrap">
-                            Téléphone
-                        </Badge>
-                        <p className="text-sm font-medium">
-                            {data.phone}
-                        </p>
-                    </div>
+            <CardContent className="p-6 pt-0 space-y-4">
+                <div className="flex items-center gap-3 p-3 bg-primary/10 rounded-md">
+                    <Badge>
+                        <Mail className="h-4 w-4 mr-2" />
+                        Email
+                    </Badge>
+                    <p className="text-sm font-medium">
+                        {data.email}
+                    </p>
+                </div>
+                <div className="flex items-center gap-3 p-3 bg-secondary/10 rounded-md">
+                    <Badge variant="secondary">
+                        <Phone className="h-4 w-4 mr-2" />
+                        Téléphone
+                    </Badge>
+                    <p className="text-sm font-medium">
+                        {data.phone}
+                    </p>
                 </div>
             </CardContent>
         </Card>
@@ -69,10 +101,11 @@ export function ClubCard({ data, setIsEditing }: Readonly<CardPropsType>) {
 const EditCard = ({ data, setIsEditing }: Readonly<CardPropsType>) => {
     return (
         <div className="relative col-span-2">
-            <Button onClick={() => setIsEditing(false)} variant="destructive" className="size-fit p-2 absolute top-0 right-0">
+            <Button onClick={() => setIsEditing(false)} variant="destructive" className="size-fit p-2 absolute top-0 right-0 z-10">
                 <X />
             </Button>
             <Form defaultValues={data} setIsEditing={setIsEditing} />
         </div>
     )
 }
+
--- src/app/(admin)/dashboard/correspondants/components/Form.tsx
+++ src/app/(admin)/dashboard/correspondants/components/Form.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { Button } from "@/components/ui/button"
 import {
     Form,
@@ -12,10 +13,12 @@ import { Input } from "@/components/ui/input"
 import { updateClub } from "../actions/server.actions"
 import { FormValues, PropsType } from "../types/form.types"
 import { useClubForm } from "../actions/client.actions"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Building2, Mail, Phone } from 'lucide-react'
 
 export default function ClubForm({ defaultValues, setIsEditing }: Readonly<PropsType>) {
-    console.log("🚀 ~ ClubForm ~ defaultValues:", defaultValues)
     const form = useClubForm(defaultValues)
+
     async function onSubmit(data: FormValues) {
         try {
             await updateClub(data)
@@ -28,58 +31,79 @@ export default function ClubForm({ defaultValues, setIsEditing }: Readonly<Props
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 bg-foreground p-5 rounded-md font-secondary">
-                <FormField
-                    control={form.control}
-                    name="libelle"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Nom du club</FormLabel>
-                            <FormControl>
-                                <Input className="font-secondary" placeholder="Cergy" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="phone"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Téléphone</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type="tel"
-                                    placeholder="06 12 34 56 78"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                        name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type="email"
-                                    placeholder="contact@cergy.fr"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit" className="w-full">
-                    {defaultValues ? "Modifier" : "Créer"}
-                </Button>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-md mx-auto">
+                <Card className="shadow-md">
+                    <CardHeader>
+                        <CardTitle className="text-2xl font-bold text-primary">
+                            {defaultValues ? "Modifier le club" : "Créer un nouveau club"}
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent className="space-y-6">
+                        <FormField
+                            control={form.control}
+                            name="libelle"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nom du club</FormLabel>
+                                    <FormControl>
+                                        <div className="relative">
+                                            <Building2 className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                                            <Input className="pl-10" placeholder="Cergy" {...field} />
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="phone"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Téléphone</FormLabel>
+                                    <FormControl>
+                                        <div className="relative">
+                                            <Phone className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                                            <Input
+                                                className="pl-10"
+                                                type="tel"
+                                                placeholder="06 12 34 56 78"
+                                                {...field}
+                                            />
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <div className="relative">
+                                            <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                                            <Input
+                                                className="pl-10"
+                                                type="email"
+                                                placeholder="contact@cergy.fr"
+                                                {...field}
+                                            />
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" className="w-full">
+                            {defaultValues ? "Modifier le club" : "Créer le club"}
+                        </Button>
+                    </CardContent>
+                </Card>
             </form>
         </Form>
     )
 }
+
